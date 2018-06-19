0a8ad00625160c148b3f917d0905016e629b23d4
ryandomo <ryanisawesome102@gmail.com>
2018-06-19 15:14:25 -0600

    Added playlist name filtering

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ class Filter extends Component {
     return (
       <div style={{defaultStyle}}>
         <img/>
-        <input type="text"/>
+        <input type="text" onKeyUp={event => this.props.onTextChange(event.target.value)}/>
       </div>
     ); 
   }
@@ -95,7 +95,8 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      serverData: {}
+      serverData: {},
+      filterString: ''
     }
   }
   componentDidMount() {
@@ -103,6 +104,7 @@ class App extends Component {
       this.setState({ serverData: fakeServerData })
     }, 1000)
   }
+
   render() {
     return (
       <div className="App">
@@ -113,8 +115,13 @@ class App extends Component {
           </h1>
           <PlaylistCounter playlists={this.state.serverData.user.playlists}/>
           <HoursCounter playlists={this.state.serverData.user.playlists}/>
-          <Filter/>
-          {this.state.serverData.user.playlists.map(playlist => 
+          <Filter onTextChange={text => {
+            this.setState({filterString: text})
+            }}/>
+          {this.state.serverData.user.playlists.filter(playlist =>
+            playlist.name.toLowerCase().includes(
+              this.state.filterString.toLowerCase())
+          ).map(playlist => 
             <Playlist playlist={playlist}/>
           )}
         </div> : <h1 style={defaultStyle}>Loading...</h1>
