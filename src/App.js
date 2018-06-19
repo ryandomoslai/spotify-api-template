a35264f2110b7517f2ebbadc5a214ec1321063e1
ryandomo <ryanisawesome102@gmail.com>
2018-06-19 11:13:46 -0600

    Added app

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+let defaultStyle = {
+  color: '#fff',
+}
+
+class Aggregate extends Component {
+  render () {
+    return (
+      <div style={{...defaultStyle, width: "40%", display: 'inline-block'}}>
+        <h2>Number Text</h2>
+      </div>
+    ); 
+  }
+}
+class Filter extends Component {
+  render() {
+    return (
+      <div style={{defaultStyle}}>
+        <img/>
+        <input type="text"/>
+      </div>
+    ); 
+  }
+}
+
+class Playlist extends Component {
+  render() {
+    return (
+      <div style={{...defaultStyle, display: 'inline-block', width: "25%"}}>
+        <img/>
+        <h3>Playlist Name</h3>
+        <ul><li>Song 1</li><li>Song 2</li><li>Song 3</li></ul>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    let name = 'Ryan'
+    let green = '#FF1212'
+    let headerStyle = {...defaultStyle, 'font-size': '50px'}
+
+    return (
+      <div className="App">
+        <h1 style={{...defaultStyle, 'font-size': '54px'}}>Title</h1>
+        <Aggregate/>
+        <Aggregate/>
+        <Filter/>
+        <Playlist/>
+        <Playlist />
+        <Playlist />
+        <Playlist />
+      </div>
+    );
+  }
+}
+
+export default App;
