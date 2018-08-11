c9822357b850dd63ecbdd2aa293ce1b2623ec131
ryandomo <ryanisawesome102@gmail.com>
2018-08-11 16:31:08 -0600

    Fixed yet another bug relating to query string

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import queryString from 'query-string'
+import queryString from 'query-string';
 
 let defaultStyle = {
   color: '#fff',
